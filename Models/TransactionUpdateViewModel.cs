3496bb4db15bd02621d185909b774fc38940c5dd
Pedro Baños <pedrobanos.renault@gmail.com>
2022-12-23 12:41:07 +0100

    some changes

---
+++ Models/TransactionUpdateViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BudjetManagement.Models
+{
+    public class TransactionUpdateViewModel: TransactionCreateViewModel
+    {
+        public int AccountPrevId { get; set; }
+        public decimal PricePrev { get; set; }
+    }
+}
---
+++ wwwroot/js/TransactionsForm.js
@@ -0,0 +1,17 @@
+﻿function initializeFormTransactions(obtainCategoriesUrl) {
+    $("#TypeOperationId").change(async function () {
+        const selectedValue = $(this).val();
+
+        const response = await fetch(obtainCategoriesUrl, {
+            method: 'POST',
+            body: selectedValue,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const json = await response.json();
+        const options = json.map(category => `<option value=${category.value}>${category.text}</option>`);
+        $("#CategoryId").html(options);
+    })
+}
